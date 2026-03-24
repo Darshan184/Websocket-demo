677e17102290508d9fbeb809db106716c22e19f8
Darshan V S <141537850+Darshan184@users.noreply.github.com>
2026-03-24 08:26:47 +0530

    Delete dist directory

--- dist/connect.d.ts
+++
@@ -1,3 +0,0 @@
-import type { APIGatewayProxyResultV2 } from "aws-lambda";
-export declare const handler: (event: any) => Promise<APIGatewayProxyResultV2>;
-//# sourceMappingURL=connect.d.ts.map
--- dist/disconnect.d.ts
+++
@@ -1,3 +0,0 @@
-import type { APIGatewayProxyWebsocketEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
-export declare const handler: (event: APIGatewayProxyWebsocketEventV2) => Promise<APIGatewayProxyResultV2>;
-//# sourceMappingURL=disconnect.d.ts.map
